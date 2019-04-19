06e2fe62191c79c42509c83df5a149a8f186194a
Tomonori0725 <queendieci0725@gmail.com>
2019-04-19 15:17:57 +0900

    refs 管理画面記事一覧絞り込み

--- sunatFramework/htdocs/admin/edit/index.php
+++ sunatFramework/htdocs/admin/edit/index.php
@@ -27,10 +27,6 @@ $ref_ss =& $ref_sess->vars;
 $tmpl_arr = array();
 $search_writer = '';
 $search_cate = '';
-$serch_date = array(
-	'start' => date("Y-m-d"),
-	'end' => null
-);
 
 $where = '';
 $arrWhere = array();
@@ -52,27 +48,90 @@ foreach($writer_list as $write){$write_id[] = $write['id'];}
 
 
 $col = 'article.id,category_name,title,date';
+$table = 'article LEFT OUTER JOIN category ON article.category_id = category.id';
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	//絞り込み検索されたら...
+	// 開始年月日のチェック
+	$start_flug = 0;
+	if(array_key_exists('start', $_POST)){
+		if(($_POST['start']['year'] != '') || ($_POST['start']['month'] != '') || ($_POST['start']['day'] != '')){
+			// 年月日いずれかが入力されていればすべて必須
+			$start_flug = FormField::NOT_NULL;
+		}
+	}
+	// 終了年月日のチェック
+	$end_flug = 0;
+	if(array_key_exists('end', $_POST)){
+		if(($_POST['end']['year'] != '') || ($_POST['end']['month'] != '') || ($_POST['end']['day'] != '')){
+			// 年月日いずれかが入力されていればすべて必須
+			$end_flug = FormField::NOT_NULL;
+		}
+	}
+
+
     //バリデーション
 	$form->post->category_id = new FormFieldSelect($cate_id);
 	$form->post->writer_id   = new FormFieldSelect($write_id);
+	$form->post->start       = new FormFieldDateTimeArray(FormField::TRIM | FormField::MBCONV | $start_flug, true);
+	$form->post->end         = new FormFieldDateTimeArray(FormField::TRIM | FormField::MBCONV | $end_flug, true);
+
 	try{
 		$ret = $form->get();
 		$cur_ss['serch'] = $ret;
 		$search_date = $cur_ss['serch']['values'];
 		$search_cate = $search_date['category_id'];
 		$search_writer = $search_date['writer_id'];
-		
+		$search_s_date = $search_date['start'];
+		$search_e_date = $search_date['end'];
+
+		//カテゴリー
 		if(!empty($search_cate)){
 			if($where) $where .= ' AND article.category_id = ' . $search_cate;
 			else  $where = 'article.category_id = ' . $search_cate;
 		}
+		//投稿者
 		if(!empty($search_writer)){
 			if($where) $where .= ' AND article.writer_id = ' . $search_writer;
 			else $where = 'article.writer_id = ' . $search_writer;
 		}
+
+		//投稿日時
+		// 開始・終了期間をDB用日付文字列へ変換する
+		$search_s_date = !is_null($search_s_date) ? $search_s_date->getValue(array('format' => 'Y-m-d')) : '0000-00-00';	// 未入力(null)時は最古
+		$search_e_date = !is_null($search_e_date) ? $search_e_date->getValue(array('format' => 'Y-m-d')) : '9999-99-99';	// 未入力(null)時は本日
+		
+		if($where) $where .= " AND '" . $search_s_date . "' <= article.date AND article.date <= '" . $search_e_date ."'";
+		else $where = "'" . $search_s_date . "' <= article.date AND article.date <= '" . $search_e_date ."'";
+
+		if($search_s_date == '0000-00-00'){
+			$startArray = array(
+				'0' => null,
+				'1' => null,
+				'2' => null
+			);
+		}else{
+			$startArray = explode('-', $search_s_date);
+		}
+		if($search_e_date == '9999-99-99'){
+			$endArray = array(
+				'0' => null,
+				'1' => null,
+				'2' => null
+			);
+		}else{
+			$endArray = explode('-', $search_e_date);
+		}
+		$serch_date = array(
+			'start_y' => $startArray[0],
+			'start_m' => $startArray[1],
+			'start_d' => $startArray[2],
+			'end_y'   => $endArray[0],
+			'end_m'   => $endArray[1],
+			'end_d'   => $endArray[2]
+		);
+		
 		if($where) $where .= ' ORDER BY article.date DESC';
+		else $where = ' ORDER BY article.date DESC';
 
 	} catch (FormCheckException $e) {
         $tmpl_arr += $e->getValues();
@@ -80,9 +139,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 }else{
 	//全記事一覧
+	$serch_date = array(
+		'start_y' => null,
+		'start_m' => null,
+		'start_d' => null,
+		'end_y'   => null,
+		'end_m'   => null,
+		'end_d'   => null
+	);
+	$table .= ' ORDER BY article.date DESC';
 }
-$table = 'article LEFT OUTER JOIN category ON article.category_id = category.id';
-if(empty($where)) $table .= ' ORDER BY article.date DESC';
+
 $article_list = $db->select($col, $table, $where, $arrWhere);
 
 $list_section = array(
@@ -99,9 +166,8 @@ $tmpl_arr += array('writer_list' => $writer_list);
 $tmpl_arr['search_cate'] = $search_cate;
 $tmpl_arr['search_writer'] = $search_writer;
 
-$tmpl_arr += array('serch_date'   => $serch_date);
 
-var_dump($tmpl_arr);
+$tmpl_arr += array('serch_date'   => $serch_date);
 
 $temp = new HTMLTemplate('admin/edit/index.html');
 echo $temp->replace($tmpl_arr);
