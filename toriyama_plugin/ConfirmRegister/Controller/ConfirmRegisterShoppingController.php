c048cd162e574a636ebf113971dbccec5f92e4ea
fujii <fujii@sunatmark.co.jp>
2019-12-10 17:33:41 +0900

    refs 確認画面で会員登録

--- toriyama_plugin/ConfirmRegister/Controller/ConfirmRegisterShoppingController.php
+++ toriyama_plugin/ConfirmRegister/Controller/ConfirmRegisterShoppingController.php
@@ -14,70 +14,36 @@
 namespace Plugin\ConfirmRegister\Controller;
 
 use Eccube\Controller\AbstractController;
-use Eccube\Entity\Customer;
 use Eccube\Entity\Master\CustomerStatus;
-use Eccube\Entity\Master\Pref;
-use Eccube\Repository\BaseInfoRepository;
-use Eccube\Repository\CustomerRepository;
-use Eccube\Repository\Master\CustomerStatusRepository;
-use Eccube\Service\MailService;
-use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class ConfirmRegisterShoppingController extends AbstractController
 {
 
-    /** @var CustomerRepository */
-    private $customerRepository;
-
-    /** @var CustomerStatusRepository */
-    protected $customerStatusRepository;
-    
-    /** @var MailService */
-    private $mailService;
-
-    /** @var BaseInfo */
-    private $BaseInfo;
-
-    /** @var EncoderFactoryInterface */
-    private $encoderFactory;
-
     /**
      * ConfirmRegisterShoppingController constructor.
-     * 
      */
-    public function __construct(
-        CustomerRepository $customerRepository,
-        CustomerStatusRepository $customerStatusRepository,
-        MailService $mailService,
-        BaseInfoRepository $baseInfoRepository,
-        EncoderFactoryInterface $encoderFactory,
-        EntityManagerInterface $entityManager
-    ) {
-        $this->customerRepository = $customerRepository;
-        $this->customerStatusRepository = $customerStatusRepository;
-        $this->mailService = $mailService;
-        $this->BaseInfo = $baseInfoRepository->get();
-        $this->encoderFactory = $encoderFactory;
+    public function __construct(EntityManagerInterface $entityManager)
+    {
         $this->entityManager = $entityManager;
     }
 
     /**
-     * 会員登録する.
+     * バリデーションチェックをする.
      *
-     * @Route("/ajax", name="regist_customer")
+     * @Route("/ajax", name="check_customer")
      * 
      * @param Request $request
      * @return Response
      * 
      */
-    public function registCustomerAction(Request $request)
+    public function checkCustomerAction(Request $request)
     {
 
         // 会員情報を配列に挿入する.
@@ -87,85 +53,22 @@ class ConfirmRegisterShoppingController extends AbstractController
         }
 
         // バリデーションチェック.
-        $error = $this->checkPassword($customer);
+        $error = $this->checkValidaton($customer);
+
         // エラーがあれば内容を返す.
         if ($error) {
             return new Response(json_encode($error));
         }
-
-        log_info('会員登録を開始する');
-
-        // 仮会員がオフかどうか.
-        $activateFlg = $this->BaseInfo->isOptionCustomerActivate();
-
-        // 都道府県を取得する.
-        $em = $this->getDoctrine()->getManager();            
-        $query = 'SELECT id FROM mtb_pref as pref WHERE pref.name = :pref';
-        $statement = $em->getConnection()->prepare($query);
-        $statement->bindValue('pref', $customer['customer_pref']);
-        $statement->execute();
-        $results = $statement->fetchAll();
-        $pref = $results[0];
-        $pref = $this->getDoctrine()->getRepository('Eccube\\Entity\\Master\\Pref')->find($results[0]['id']);
-
-        // 会員登録をする.
-        $new_customer = $this->getDoctrine()->getRepository(Customer::class)->newCustomer();
-        $new_customer
-            ->setName01($customer['customer_name01'])
-            ->setName02($customer['customer_name02'])
-            ->setKana01($customer['customer_kana01'])
-            ->setKana02($customer['customer_kana02'])
-            ->setCompanyName($customer['customer_company_name'])
-            ->setEmail($customer['email'])
-            ->setPhonenumber($customer['customer_phone_number'])
-            ->setPostalcode($customer['customer_postal_code'])
-            ->setPref($pref)
-            ->setAddr01($customer['customer_addr01'])
-            ->setAddr02($customer['customer_addr02'])
-            ->setPassword($customer['password']);
-
-        // 仮会員がオフになっている時.
-        if (!$activateFlg) {
-            $customerStatus = $this->customerStatusRepository->find(CustomerStatus::REGULAR);
-            $new_customer->setStatus($customerStatus);
-        }
-
-        log_info('パスワード暗号化');
-        // パスワードを暗号化.
-        $encoder = $this->encoderFactory->getEncoder($new_customer);
-        $salt = $encoder->createSalt();
-        $password = $encoder->encodePassword($new_customer->getPassword(), $salt);
-        $secretKey = $this->customerRepository->getUniqueSecretKey();
-
-        // 暗号化したパスワードをセット.
-        $new_customer
-            ->setSalt($salt)
-            ->setPassword($password)
-            ->setSecretKey($secretKey)
-            ->setPoint(0);
-
-        log_info('DBに追加');
-        $this->entityManager->persist($new_customer);
-        $this->entityManager->flush();
-        log_info('値を返す');
-
-        // 仮会員用のメールを送信する.
-        $activateUrl = $this->generateUrl('entry_activate', ['secret_key' => $new_customer->getSecretKey()], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        // メールを送信する.
-        if ($activateFlg) {
-            // 仮会員メールを送信.
-            $this->mailService->sendCustomerConfirmMail($new_customer, $activateUrl);
-        } else {
-            // 会員登録完了メールを送信.
-            $this->mailService->sendCustomerCompleteMail($new_customer, $activateUrl);
-        }
+        // エラーがなければセッションに登録
+        $_SESSION['regist_password'] = $customer['password'];
 
         return new Response(null);
     }
 
-
-    public function checkPassword($customer){
+    /**
+     * バリデーションチェック.
+     */
+    public function checkValidaton($customer){
         $errorList = '';
 
         // 半角英数記号8～32文字.
@@ -189,6 +92,4 @@ class ConfirmRegisterShoppingController extends AbstractController
 
         return false;
     }
-
-
 }
--- toriyama_plugin/ConfirmRegister/Event.php
+++ toriyama_plugin/ConfirmRegister/Event.php
@@ -1,23 +1,85 @@
 <?php
 
 namespace Plugin\ConfirmRegister;
-
+use Psr\Container\ContainerInterface;
+use Eccube\Entity\Customer;
+use Eccube\Entity\Master\Pref;
+use Eccube\Entity\Master\CustomerStatus;
+use Eccube\Repository\BaseInfoRepository;
+use Eccube\Repository\CustomerRepository;
+use Eccube\Repository\OrderRepository;
+use Eccube\Repository\Master\CustomerStatusRepository;
+use Eccube\Repository\Master\PrefRepository;
+use Eccube\Service\MailService;
+use Doctrine\ORM\EntityManagerInterface;
 use Eccube\Event\TemplateEvent;
+use Eccube\Event\EccubeEvents;
+use Eccube\Event\EventArgs;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
-use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\Form\FormEvents;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\Security\Core\Encoder\EncoderFactoryInterface;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class Event implements EventSubscriberInterface
 {
+    /** @var ContainerInterface */
+    protected $container;
+
+    /** @var BaseInfo */
+    private $BaseInfo;
+
+    /** @var CustomerStatusRepository */
+    protected $customerStatusRepository;
+
+    /** @var PrefStatusRepository */
+    protected $prefRepository;
+
+    /** @var CustomerRepository */
+    private $customerRepository;
+    
+    /** @var OrderRepository */
+    private $orderRepository;
+    
+    /** @var MailService */
+    private $mailService;
+
+    /** @var EncoderFactoryInterface */
+    private $encoderFactory;
+
+    public function __construct(
+        ContainerInterface $container,
+        BaseInfoRepository $baseInfoRepository,
+        CustomerRepository $customerRepository,
+        CustomerStatusRepository $customerStatusRepository,
+        PrefRepository $prefRepository,
+        OrderRepository $orderRepository,
+        EncoderFactoryInterface $encoderFactory,
+        EntityManagerInterface $entityManager,
+        MailService $mailService
+    ) {
+        $this->container = $container;
+        $this->BaseInfo = $baseInfoRepository->get();
+        $this->customerRepository = $customerRepository;
+        $this->customerStatusRepository = $customerStatusRepository;
+        $this->prefRepository = $prefRepository;
+        $this->orderRepository = $orderRepository;
+        $this->encoderFactory = $encoderFactory;
+        $this->entityManager = $entityManager;
+        $this->mailService = $mailService;
+    }
+
+
     /**
      * @return array
      */
     public static function getSubscribedEvents()
     {
         return [
-            'Shopping/confirm.twig' => 'index'
+            'Shopping/confirm.twig' => 'index',
+            EccubeEvents::FRONT_SHOPPING_COMPLETE_INITIALIZE => 'onFrontShoppingCompleteInitialize',
         ];
     }
 
@@ -27,6 +89,81 @@ class Event implements EventSubscriberInterface
     public function index(TemplateEvent $event)
     {
         $event->addSnippet('@ConfirmRegister/default/confirm_register_shopping_item.twig');
+    }
+
+    /**
+     * 注文完了画面のevent hock.
+     * 会員登録をし、メールを送信する.
+     *
+     * @param TemplateEvent $event
+     */
+    public function onFrontShoppingCompleteInitialize(EventArgs $event)
+    {
+        // 仮会員がオフかどうか.
+        $activateFlg = $this->BaseInfo->isOptionCustomerActivate();
+
+        // 都道府県を取得する.
+        $pref = $this->prefRepository->findBy(array('id' => $_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['Pref']['id']));
+
+        // 会員登録をする.
+        $new_customer = new Customer();
+        $new_customer
+            ->setName01($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['name01'])
+            ->setName02($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['name02'])
+            ->setKana01($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['kana01'])
+            ->setKana02($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['kana02'])
+            ->setCompanyName($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['company_name'])
+            ->setEmail($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['email'])
+            ->setPhonenumber($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['phone_number'])
+            ->setPostalcode($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['postal_code'])
+            ->setPref($pref[0])
+            ->setAddr01($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['addr01'])
+            ->setAddr02($_SESSION["_sf2_attributes"]['eccube.front.shopping.nonmember']['addr02'])
+            ->setPassword($_SESSION['regist_password']);
+
+        // 仮会員がオフの時に本会員にする.
+        if (!$activateFlg) {
+            $customerStatus = $this->customerStatusRepository->find(CustomerStatus::REGULAR);
+            $new_customer->setStatus($customerStatus);
+        } else {
+            $customerStatus = $this->customerStatusRepository->find(CustomerStatus::NONACTIVE);
+            $new_customer->setStatus($customerStatus);
+        }
+
+        // パスワードを暗号化.
+        $encoder = $this->encoderFactory->getEncoder($new_customer);
+        $salt = $encoder->createSalt();
+        $password = $encoder->encodePassword($new_customer->getPassword(), $salt);
+        $secretKey = $this->customerRepository->getUniqueSecretKey();
+
+        // 暗号化したパスワードをセット.
+        $new_customer
+            ->setSalt($salt)
+            ->setPassword($password)
+            ->setSecretKey($secretKey)
+            ->setPoint(0);
+
+        // 会員登録をする.
+        $this->entityManager->persist($new_customer);
+        $this->entityManager->flush();
+
+        // 注文履歴に会員情報を紐づける.
+        $order = $this->orderRepository->findOneById(array('id' => $_SESSION["_sf2_attributes"]["eccube.front.shopping.order.id"]));
+        $order->setCustomer($new_customer);
+        $this->entityManager->flush();
+
+        // 仮会員用のメールを送信する.
+        $activateUrl = $this->container->get('router')->generate('entry_activate', ['secret_key' => $new_customer->getSecretKey()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        // メールを送信する.
+        if ($activateFlg) {
+            // 仮会員メールを送信.
+            $this->mailService->sendCustomerConfirmMail($new_customer, $activateUrl);
+        } else {
+            // 会員登録完了メールを送信.
+            $this->mailService->sendCustomerCompleteMail($new_customer, $activateUrl);
+        }
+
     }
 
 }
